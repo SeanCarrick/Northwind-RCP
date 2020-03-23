ea19bac3e1bcbf9b175e8f218cf46b46a50377b6
Sean Carrick <sean@pekinsoft.com>
2020-03-22 23:05:43 -0500

    Accounting Module Started
    I have started working on mocking up the `Northwind-Accounting` module. I am going to create the visual elements to give us an idea of how the system will look.
    
    I need to Open an Issue for the "Chart of Accounts" window, though. I have it set to be an explorer window, but it keeps opening in the editor area. I have tried changing it to a properties window, but that did not fix the problem either. I am not sure what to make of this. Maybe you could look at it @jkovalsky and see if I am missing something.
    Signed-off-by:Sean Carrick <sean@pekinsoft.com>

---
+++ Northwind-RCP/Northwind-Accounting/src/com/pekinsoft/northwind/accounting/controller/CoAViewAction.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pekinsoft.northwind.accounting.controller;
+
+import com.pekinsoft.northwind.accounting.view.CoATopComponent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Window",
+        id = "com.pekinsoft.northwind.accounting.controller.CoAViewAction"
+)
+@ActionRegistration(
+        iconBase = "com/pekinsoft/northwind/accounting/controller/Clipboard.png",
+        displayName = "#CTL_CoAViewAction"
+)
+@ActionReferences({
+    @ActionReference(path = "Menu/Window/Optional Tools", position = 3333),
+    @ActionReference(path = "Shortcuts", name = "WINDOWS F5")
+})
+@Messages("CTL_CoAViewAction=Chart of Accounts")
+public final class CoAViewAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Create and show the Chart of Accounts viewer
+        CoATopComponent coa = new CoATopComponent();
+        coa.open();
+    }
+}
